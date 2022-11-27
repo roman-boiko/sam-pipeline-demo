ed82bcb82d128f5a2d18b65d258f2e821cb99920
Roman Boiko <roman.boiko@gmail.com>
2022-11-27 15:29:36 -0800

    reinvent

--- hello-world/app.ts
+++ hello-world/app.ts
@@ -15,7 +15,7 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGat
         return {
             statusCode: 200,
             body: JSON.stringify({
-                message: 'hello re:invent',
+                message: 'hello re:reinvent22',
             }),
         };
     } catch (err) {
